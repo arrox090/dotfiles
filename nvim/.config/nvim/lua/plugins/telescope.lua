6554d7b19e5722757d439ec5bd372299d3229ba2
arrox090 <jan.olechnowicz090@gmail.com>
2025-09-21 17:02:30 +0200

    Added indent-blankline and auto-session plugins, disabled lazy.nvim update notifications and disabled notify plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,49 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"andrew-george/telescope-themes",
+		},
+		config = function()
+			local builtin_schemes = require("telescope._extensions.themes").builtin_schemes
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					layout_config = {
+						horizontal = {
+							width = 0.95,
+							height = 0.9,
+							preview_width = 0.7,
+						},
+					},
+					enable_previewer = true,
+					enable_live_preview = false,
+					persist = { enabled = true },
+				},
+			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>en", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "Telescope live grep" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
