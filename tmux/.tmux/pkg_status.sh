655770c570287b649f2ac0cce8dea5a71aac2a60
arrox090 <jan.olechnowicz090@gmail.com>
2026-04-09 12:50:30 +0200

    Fixed tmux constantly running update command on yay/brew causing rate limits, made it fetch only once an hour

--- tmux/.tmux/pkg_status.sh
+++ tmux/.tmux/pkg_status.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-if uname | grep -q Darwin; then
-  # macOS → brew
-  echo "#[bg=#{@thm_bg},fg=#{@thm_mauve}] ⚡ $(brew outdated --quiet | wc -l | tr -d ' \n') brew "
+CACHE_FILE="/tmp/tmux_package_update_count"
+CACHE_TIME=3600 # update every hour
+
+if [ -f "$CACHE_FILE"] && [ $(($(date +%s) - $(stat -c %Y "$CACHE_FILE"))) -lt $CACHE_TIME ]; then
+  UPDATES=$(cat "$CACHE_FILE")
 else
-  # Arch Linux → yay
-  echo "#[bg=#{@thm_bg},fg=#{@thm_blue}]  $(yay -Qu 2>/dev/null | wc -l | tr -d ' \n') yay "
+  if uname | grep -q Darwin; then
+    # macOS → brew
+    UPDATES=$(brew outdated --quiet | wc -l | tr -d ' \n')
+  else
+    # Arch Linux → yay
+    UPDATES=$(yay -Qu 2>/dev/null | wc -l | tr -d ' \n')
+  fi
+
+  echo "$UPDATES" >"$CACHE_FILE"
+fi
+
+if uname | grep -q Darwin; then
+  echo "#[bg=#{@thm_bg},fg=#{@thm_mauve}] ⚡ ${UPDATES} brew "
+else
+  echo "#[bg=#{@thm_bg},fg=#{@thm_blue}]  ${UPDATES} yay "
 fi
